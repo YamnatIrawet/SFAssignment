4cc4670b08a5ebc382bfea6742fb7482acc9791f
YamnatIrawet <YamnatIrawet@gmail.com>
2021-04-29 19:46:12 +0530

    final for 29April21

--- force-app/main/default/aura/NavChild_cmp/NavChild_cmpController.js
+++ force-app/main/default/aura/NavChild_cmp/NavChild_cmpController.js
@@ -1,7 +1,11 @@
 ({
     init: function(cmp, event, helper) {
+        console.log('I came in child controller......1');
         var pageReference = cmp.get("v.pageReference");
+        console.log('I came in child controller......2');
         cmp.set("v.firstName", pageReference.state.firstName);
+        console.log('first name: '+pageReference.state.firstName);
         cmp.set("v.lastName", pageReference.state.lastName);
+        console.log('I came in child controller......3');
     }
 })
--- force-app/main/default/aura/NavParent_cmp/NavParent_cmpController.js
+++ force-app/main/default/aura/NavParent_cmp/NavParent_cmpController.js
@@ -8,21 +8,25 @@
                 "componentName": "c__NavChild_cmp"    
             },    
             "state": {
-                "firstName": "Test"  ,
-                "lastName": "user"    
+                "c__firstName": "Test"  ,
+                "c__lastName": "user"    
             }
         };
         cmp.set("v.pageReference", pageReference);
         var defaultUrl = "#";
+        console.log('I came in parent init controller.....1');
         navService.generateUrl(pageReference)
         .then($A.getCallback(function(url) {
+            console.log('I came in parent init controller.....2');
             cmp.set("v.url", url ? url : defaultUrl);
         }), $A.getCallback(function(error) {
+            console.log('I came in parent init controller.....3');
             cmp.set("v.url", defaultUrl);
         }));
     },
     handleClick: function(cmp, event, helper) {
         var navService = cmp.find("navService");
+        console.log('I came in click of parent controller');
         // Uses the pageReference definition in the init handler
         var pageReference = cmp.get("v.pageReference");
         event.preventDefault();
